d20bf9a24d7cf15827c01c2c248cf97d6f917019
Nethmika Broo <92091962+darshanamk@users.noreply.github.com>
2021-10-25 06:32:12 +0530

    Update maker.js

--- plugins/maker.js
+++ plugins/maker.js
@@ -2,7 +2,7 @@
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 WHITE DEVIL ----»»» TERROR BOY
-re edited by BADBMODER
+re edited by NETHMIKA BROO
 Badboy
 */
 
@@ -18,7 +18,7 @@ Ktb.addCommand({pattern: 'glue ?(.*)', fromMe: false, dontAddCommandList: true},
 
     var webimage = await axios.get(`https://zenzapi.xyz/api/textpro/gluetext?text=${match[1]}&apikey=b91fbfa191`, { responseType: 'arraybuffer' })
 
-  await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg , caption: '*𝙼𝙰𝙳𝙴 𝙱𝚈 𝙱𝙰𝙳𝙱𝙾𝚈🤪*'})
+  await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg , caption: '*𝙼𝚊𝚍𝚎 𝙱𝚢 𝙽𝚎𝚝𝚑𝚖𝚒𝚔𝚊 𝙱𝚛𝚘𝚘🙈*'})
 
 }));
 
@@ -29,7 +29,7 @@ Ktb.addCommand({pattern: 'wasted ?(.*)', fromMe: false, dontAddCommandList: true
 
     var webimage = await axios.get(`https://zenzapi.xyz/api/image/wasted?image=${match[1]}&apikey=b91fbfa191`, { responseType: 'arraybuffer' })
 
-  await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg , caption: '*𝙼𝙰𝙳𝙴 𝙱𝚈 𝙱𝙰𝙳𝙱𝙾𝚈🤪*'})
+  await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg , caption: '*𝙼𝚊𝚍𝚎 𝙱𝚢 𝙽𝚎𝚝𝚑𝚖𝚒𝚔𝚊 𝙱𝚛𝚘𝚘🙈*'})
 
 }));
 
@@ -40,6 +40,6 @@ Ktb.addCommand({pattern: 'delete ?(.*)', fromMe: false, dontAddCommandList: true
 
     var webimage = await axios.get(`https://zenzapi.xyz/api/image/delete?image=${match[1]}&apikey=b91fbfa191`, { responseType: 'arraybuffer' })
 
-  await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg , caption: '*𝙼𝙰𝙳𝙴 𝙱𝚈 𝙱𝙰𝙳𝙱𝙾𝚈🤪*'})
+  await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg , caption: '*𝙼𝚊𝚍𝚎 𝙱𝚢 𝙽𝚎𝚝𝚑𝚖𝚒𝚔𝚊 𝙱𝚛𝚘𝚘🙈*'})
 
 }));
