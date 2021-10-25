4b85fac580fddd2527e9ec2ebd2da23b2bdb7091
Nethmika Broo <92091962+darshanamk@users.noreply.github.com>
2021-10-25 06:52:55 +0530

    Update memes.js

--- plugins/memes.js
+++ plugins/memes.js
@@ -6,7 +6,7 @@ you may not use this file except in compliance with the License.
 WhatsAsena - Yusuf Usta
 Coded by @KursadHD
 
-re edited by BADBMODER
+re edited by Nethmika Broo
 */
 
 const MyPnky  = require('../events');
